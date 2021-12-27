ca35b705dd6e09bff9d48b08ef5004c97ac0bf41
canbitmis <voscan46@gmail.com>
2021-12-27 20:54:16 +0300

    update

--- src/functions.js
+++ src/functions.js
@@ -38,8 +38,70 @@ sayi1=sayi2;
 sayi2=100;
 console.log(sayi1);
 
+function addtoCart4(x) {
+    console.log(products)
+}
+ 
+let products = [
+    {productName:"Gitar", unitPrice:900,quantity:3},
+    {productName:"Pena", unitPrice:5,quantity:200},
+    {productName:"Kapo", unitPrice:55,quantity:4}
+]
+addtoCart4(products)
 
 
 
+/*function add(...numbers) { //rest
+    for (let i = 0; i < numbers.length; i++) {
+        console.log(numbers[i])
+        
+    }  
+} */
+
+function add(...numbers){
+   let total= 0;
+   for (let i =0 ; i < numbers.length ; i++){
+       total= total+numbers[i]
+   }
+       console.log("toplam = "+total)
+}
+
+add(20,30)
+//add(20,30,40)
+//add(20,30,40,50)
+
+console.log("En Büyük :" + Math.max(30,56,98,77,31,65,49,856,32,65)) 
+
+let numbers =[30,10,500,600,120]
+console.log(numbers)
+console.log(...numbers)
+console.log(Math.max(...numbers))
+
+
+/*let musics = ["Rock","Jazz", "Classic"];
+console.log(musics[2]) */
+
+let [rock,jazz] = [
+    "Rock",
+    "Jazz",
+    "Classic"
+]
+console.log(rock)
+console.log(jazz)
+
+
+let [icAnadolu,marmara] = [
+    {name : "İç Anadolu" , population : "30M"},
+    {name : "Marmara" , population : "20M"},
+    {name : "Karadeniz" , population :"10M"}
+]
+console.log(icAnadolu)
+console.log(marmara)
+
+
+let newProductName , newUnitPrice , newQuantity
+{productName:newProductName , unitPrice:newUnitPrice , quantity:newQuantity
+=productName:"Elma" , unitPrice:10 , quantity;15}
+console.log(newProductName)
 
 
